a9ea62eeb74631a767a35539d49f139f9b7e020e
Leonardo Marinho <leonardo.marinho@gmail.com>
2018-09-15 00:24:31 -0300

    Initial commit

---
+++ src/main/java/br/com/deutilt/gympasschallenge/GympassChallengeApplication.java
@@ -0,0 +1,12 @@
+package br.com.deutilt.gympasschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GympassChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GympassChallengeApplication.class, args);
+	}
+}
