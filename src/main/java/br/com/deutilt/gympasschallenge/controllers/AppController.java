7fe9017715d1c548aceded13913cec35458ec8ce
Leonardo Marinho <leonardo.marinho@gmail.com>
2018-09-17 01:59:41 -0300

    Including CORS access control headers

--- src/main/java/br/com/deutilt/gympasschallenge/controllers/AppController.java
+++ src/main/java/br/com/deutilt/gympasschallenge/controllers/AppController.java
@@ -5,11 +5,9 @@ import br.com.deutilt.gympasschallenge.interfaces.IResultService;
 import br.com.deutilt.gympasschallenge.models.dtos.ResultDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
@@ -25,9 +23,11 @@ public class AppController {
         this.resultService = resultService;
     }
 
-    @PostMapping("/")
+    @CrossOrigin
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
+    @RequestMapping(value = "/", method = RequestMethod.POST, consumes = {
+            "multipart/form-data", MediaType.APPLICATION_JSON_VALUE })
     public ResultDTO handleFileUpload(@RequestParam("file") MultipartFile file){
         return resultService.getResult(fileService.getFromFile(file));
     }
